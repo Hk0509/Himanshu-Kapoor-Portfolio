fcc038a06c340abf435d4b5f7c2c76de34315f88
Hk0509 <himanshukapoor898@gmail.com>
2024-02-07 00:39:19 +0530

    new commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -31,11 +31,6 @@ const Contact = () => {
 
   const isInView = useInView(ref, { margin: "-100px" });
 
-  const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
-  const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
-  const userId = process.env.REACT_APP_EMAILJS_USER_ID;
-
-
   const sendEmail = (e) => {
     e.preventDefault();
   
