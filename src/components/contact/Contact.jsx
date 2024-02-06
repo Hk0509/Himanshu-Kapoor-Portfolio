2cc21b55b538ce0a81cd811e1543347dfa8264f4
Hk0509 <himanshukapoor898@gmail.com>
2024-02-07 00:49:38 +0530

    new commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -19,16 +19,17 @@ const variants = {
   },
 };
 
-const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
-const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
-const userId = process.env.REACT_APP_EMAILJS_USER_ID;
 
 const Contact = () => {
   const ref = useRef();
   const formRef = useRef();
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
-
+  
+  const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+  const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+  const userId = process.env.REACT_APP_EMAILJS_USER_ID;
+  
   const isInView = useInView(ref, { margin: "-100px" });
 
   const sendEmail = (e) => {
