9b30be9b0d08f7725606f97e535eb555a49967d3
Hk0509 <himanshukapoor898@gmail.com>
2024-02-07 00:38:24 +0530

    new commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,6 +4,10 @@ import { motion, useInView } from "framer-motion";
 import emailjs from "@emailjs/browser";
 import emailjsConfig from '../../../emailjsConfig.env';
 
+const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+const userId = process.env.REACT_APP_EMAILJS_USER_ID;
+
 const variants = {
   initial: {
     y: 500,
